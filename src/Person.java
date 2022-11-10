6562daa52f67587742fce4f82325227a678e34d3
madina1212 <halikovamadina656@gmail.com>
2022-11-10 16:08:55 +0600

    Initial commit

---
+++ src/Person.java
@@ -0,0 +1,52 @@
+public class Person{
+    protected String name;
+    String designation;
+    private String age;
+
+    public Person(String name, String designation) {
+        this.name = name;
+        this.designation = designation;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDesignation() {
+        return designation;
+    }
+
+    public void setDesignation(String designation) {
+        this.designation = designation;
+    }
+
+    public void learn() {
+        System.out.println("LEARN");
+    }
+    public void walk() {
+        System.out.println("WALK");
+    }
+    public void eat() {
+        System.out.println("EAT");
+    }
+
+    @Override
+    public String toString() {
+        return "Person{" +
+                "name='" + name + '\'' +
+                ", designation='" + designation + '\'' +
+                '}';
+    }
+
+    public String getAge() {
+        return age;
+    }
+
+    public void setAge(String age) {
+        this.age = age;
+    }
+}
